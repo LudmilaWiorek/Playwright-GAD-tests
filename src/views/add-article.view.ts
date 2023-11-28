f0a86ab9181cb949886408e4c3925eb9d74bc9aa
Ludmiła Wiorek <ludmila.wiorek@gmail.com>
2023-11-28 11:44:22 +0100

    tests: negative tests for creating article

--- src/views/add-article.view.ts
+++ src/views/add-article.view.ts
@@ -6,6 +6,7 @@ export class AddArticleView {
   titleInput = this.page.getByTestId('title-input')
   bodyInput = this.page.getByTestId('body-text')
   saveButton = this.page.getByTestId('save')
+  messageCannotAddArticle = this.page.locator('#alertPopup')
 
   constructor(private page: Page) {}
 
--- tests/articles.spec.ts
+++ tests/articles.spec.ts
@@ -21,13 +21,63 @@ test.describe('Verify articles', () => {
     await expect.soft(addArticleView.header).toBeVisible()
 
     const articleData = randomNewArticle()
-
     await addArticleView.createArticle(articleData)
+
     //Assert
     const articlePage = new ArticlePage(page)
     await expect.soft(articlePage.articleTitle).toHaveText(articleData.title)
     await expect
       .soft(articlePage.articleBody)
       .toHaveText(articleData.body, { useInnerText: true })
+  })
+  // two negative tests below - failed to create new article not filling title/body fields - are own work in the course
+  test('unsuccessful creating new article @GAD_R04_01 with empty title', async ({
+    page,
+  }) => {
+    //Arrange
+    const loginPage = new LoginPage(page)
+    await loginPage.goto()
+    await loginPage.login(testUser2)
+
+    const articlesPage = new ArticlesPage(page)
+    await articlesPage.goto()
+    //Act
+    await articlesPage.addArticleButtonLogged.click()
+
+    //Assert
+    const addArticleView = new AddArticleView(page)
+    await expect.soft(addArticleView.header).toBeVisible()
+    const articleData = randomNewArticle()
+    articleData.title = ''
+    await addArticleView.createArticle(articleData)
+    await expect(addArticleView.messageCannotAddArticle).toBeVisible()
+    await expect(addArticleView.messageCannotAddArticle).toHaveText(
+      'Article was not created',
+    )
+  })
+  test('unsuccessful creating new article @GAD_R04_01 with empty body', async ({
+    page,
+  }) => {
+    //Arrange
+    const loginPage = new LoginPage(page)
+    await loginPage.goto()
+    await loginPage.login(testUser2)
+
+    const articlesPage = new ArticlesPage(page)
+    await articlesPage.goto()
+    //Act
+    await articlesPage.addArticleButtonLogged.click()
+
+    //Assert
+    const addArticleView = new AddArticleView(page)
+    await expect.soft(addArticleView.header).toBeVisible()
+    const articleData = randomNewArticle()
+    articleData.body = ''
+    await addArticleView.createArticle(articleData)
+
+    await expect(addArticleView.messageCannotAddArticle).toBeVisible()
+    await expect(addArticleView.messageCannotAddArticle).toHaveText(
+      'Article was not created',
+    )
   })
 })
