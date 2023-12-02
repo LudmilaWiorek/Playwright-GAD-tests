d780210f8c15dd64fb0cfd4dc9cd9bc0798abc89
Ludmiła Wiorek <ludmila.wiorek@gmail.com>
2023-12-02 15:17:00 +0100

    chore: locator name refactored

--- tests/articles.spec.ts
+++ tests/articles.spec.ts
@@ -21,7 +21,7 @@ test.describe('Verify articles', () => {
     await articlesPage.goto()
     await articlesPage.addArticleButtonLogged.click()
 
-    await expect.soft(addArticleView.header).toBeVisible()
+    await expect.soft(addArticleView.addNewHeader).toBeVisible()
   })
 
   test('reject creating new article @GAD_R04_01 with empty title', async () => {
