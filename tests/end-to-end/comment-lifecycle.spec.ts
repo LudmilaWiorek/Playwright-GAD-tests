819ad47c03517074a92312297db428406d4fd197
Ludmiła Wiorek <ludmila.wiorek@gmail.com>
2023-12-12 15:28:33 +0100

    test: second comment test (step)

--- tests/end-to-end/comment-lifecycle.spec.ts
+++ tests/end-to-end/comment-lifecycle.spec.ts
@@ -40,7 +40,7 @@ test.describe('Create, verify and delete comment', () => {
     await addArticleView.createArticle(articleData)
   })
 
-  test('operate on comments @GAD_R05_01', async () => {
+  test('operate on comments @GAD_R05_01, @GAD-R05-02, @GAD-R05-03', async () => {
     const expectedCommentCreatedPopup = 'Comment was created'
 
     const newCommentData = prepareRandomComment()
@@ -103,6 +103,22 @@ test.describe('Create, verify and delete comment', () => {
       )
       //Assert
       await expect(updatedArticleComment.body).toHaveText(editCommentData.body)
+    })
+
+    //5. TEST STEP for requirement: user can add more than one comment
+    await test.step('create and verify second comment', async () => {
+      const secondCommentData = prepareRandomComment()
+      //Act
+      await articlePage.addCommentButton.click()
+      await addCommentView.createComment(secondCommentData)
+
+      //Assert
+      const articleComment = articlePage.getArticleComment(
+        secondCommentData.body,
+      )
+      await expect(articleComment.body).toHaveText(secondCommentData.body)
+      await articleComment.link.click()
+      await expect(commentPage.commentBody).toHaveText(secondCommentData.body)
     })
   })
 })
