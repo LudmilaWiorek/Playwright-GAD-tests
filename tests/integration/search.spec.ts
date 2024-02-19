18e100838caa696f292df427b283ea4b8c949ceb
Ludmiła Wiorek <ludmila.wiorek@gmail.com>
2024-02-19 11:18:06 +0100

    test: go button action (first API test)

---
+++ tests/integration/search.spec.ts
@@ -0,0 +1,20 @@
+import { expect, test } from '@_src/fixtures/merge.fixture'
+
+test.describe('Verify search component for articles', () => {
+  test('go button should fetch articles @GAD-R07-01', async ({
+    articlesPage,
+    page,
+  }) => {
+    // Arrange
+    await expect(articlesPage.goSearchButton).toBeInViewport()
+    const responsePromise = page.waitForResponse('/api/articles*')
+    const expectDefaultArticleNumber = 6
+    // Act
+    await articlesPage.goSearchButton.click()
+    const response = await responsePromise
+    const body = await response.json()
+    // Assert
+    expect(response.ok()).toBeTruthy() //method ok - check if response code is between 200-299
+    expect(body).toHaveLength(expectDefaultArticleNumber)
+  })
+})
