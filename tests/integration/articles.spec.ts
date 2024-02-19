8efd00af0233a687b35913f8a594ab3ddf18b91f
Ludmiła Wiorek <ludmila.wiorek@gmail.com>
2024-02-19 12:22:08 +0100

    chore: backend verification for test

--- tests/integration/articles.spec.ts
+++ tests/integration/articles.spec.ts
@@ -1,19 +1,30 @@
+import { RESPONSE_TIMEOUT } from '@_pw-config'
 import { prepareRandomNewArticle } from '@_src/factories/article.factory'
 import { expect, test } from '@_src/fixtures/merge.fixture'
 
 test.describe('Verify articles', () => {
   test('reject creating new article without title @GAD_R04_01 @logged', async ({
     addArticleView,
+    page,
   }) => {
     //Arrange
     const expectedErrorMessage = 'Article was not created'
     const articleData = prepareRandomNewArticle()
     articleData.title = ''
+
+    //testing API here!!
+    const responsePromise = page.waitForResponse('api/articles', {
+      timeout: RESPONSE_TIMEOUT,
+    }) //added timeout to cut timing of waiting (added also in pw.config)
+    const expectedResponseCode = 422
+
     //Act
     await addArticleView.createArticle(articleData)
+    const response = await responsePromise
 
     //Assert
     await expect(addArticleView.alertPopUp).toHaveText(expectedErrorMessage)
+    expect(response.status()).toBe(expectedResponseCode)
   })
 
   test('reject creating new article without body @GAD_R04_01  @logged', async ({
@@ -23,6 +34,7 @@ test.describe('Verify articles', () => {
     const expectedErrorMessage = 'Article was not created'
     const articleData = prepareRandomNewArticle()
     articleData.body = ''
+
     //Act
     await addArticleView.createArticle(articleData)
 
@@ -35,6 +47,7 @@ test.describe('Verify articles', () => {
     test('reject creating article with title exceeding 128 signs @GAD_R04_02  @logged', async ({
       addArticleView,
     }) => {
+      //Arrange
       const expectedErrorMessage = 'Article was not created'
       const articleData = prepareRandomNewArticle(129)
 
@@ -48,6 +61,7 @@ test.describe('Verify articles', () => {
     test('create article with title equals 128 signs @GAD_R04_02  @logged', async ({
       addArticleView,
     }) => {
+      //Arrange
       const articleData = prepareRandomNewArticle(128)
 
       // Act
