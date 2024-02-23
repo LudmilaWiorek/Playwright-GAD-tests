ec807f0962d18012a7c319e3bb9d6f9a7db10cff
Ludmiła Wiorek <ludmila.wiorek@gmail.com>
2024-02-23 14:49:53 +0100

    test: api verification after comment creation

--- src/utils/wait.util.ts
+++ src/utils/wait.util.ts
@@ -4,6 +4,21 @@ import { Page, Response } from '@playwright/test'
 export async function waitForResponse(
   page: Page,
   url: string,
+  method?: string,
+  status?: number,
 ): Promise<Response> {
-  return page.waitForResponse(url, { timeout: RESPONSE_TIMEOUT })
+  return page.waitForResponse(
+    (response) => {
+      //return is about to return true or false
+      return (
+        response.url().includes(url) &&
+        (!method || response.request().method() == method) &&
+        (!status || response.status() == status)
+      )
+    },
+
+    {
+      timeout: RESPONSE_TIMEOUT,
+    },
+  )
 }
--- tests/integration/comment.spec.ts
+++ tests/integration/comment.spec.ts
@@ -1,36 +1,30 @@
-import { RESPONSE_TIMEOUT } from '@_pw-config'
 import { prepareRandomComment } from '@_src/factories/comment.factory'
 import { expect, test } from '@_src/fixtures/merge.fixture'
+import { waitForResponse } from '@_src/utils/wait.util'
 
-// here should be describe 'Verify comments'; imo its not necessary, maybe i add it in future :)
-test('should return created comment - API validation @GAD_R07_06 @logged', async ({
-  createRandomArticle,
-  page,
-}) => {
-  //Arrange
-  const expectedCommentCreatedPopup = 'Comment was created'
+test.describe('Verify comment', () => {
+  test('should return created comment - API validation @GAD_R07_06 @logged', async ({
+    createRandomArticle,
+    page,
+  }) => {
+    //Arrange
+    const expectedCommentCreatedPopup = 'Comment was created'
 
-  const newCommentData = prepareRandomComment()
-  let articlePage = createRandomArticle.articlePage
-  const addCommentView = await articlePage.clickAddCommentButton()
+    const newCommentData = prepareRandomComment()
+    let articlePage = createRandomArticle.articlePage
+    const addCommentView = await articlePage.clickAddCommentButton()
 
-  //Act
-  articlePage = await addCommentView.createComment(newCommentData)
-  const responsePromise = page.waitForResponse(
-    (response) => {
-      return (
-        response.url().includes('api/comments') &&
-        response.request().method() == 'GET'
-      )
-    },
-    { timeout: RESPONSE_TIMEOUT },
-  )
-  const response = await responsePromise
+    //Act
+    articlePage = await addCommentView.createComment(newCommentData)
+    const responsePromise = waitForResponse(page, '/api/comments', 'GET')
 
-  //Assert
-  await expect
-    .soft(articlePage.alertPopup)
-    .toHaveText(expectedCommentCreatedPopup)
+    const response = await responsePromise
 
-  expect(response.ok()).toBeTruthy()
+    //Assert
+    await expect
+      .soft(articlePage.alertPopup)
+      .toHaveText(expectedCommentCreatedPopup)
+
+    expect(response.ok()).toBeTruthy()
+  })
 })
