5a16d82c8624d01833cc1d012bf5573fedff80ca
Ludmiła Wiorek <ludmila.wiorek@gmail.com>
2024-07-12 15:52:59 +0200

    chore: tag added for api test (also corrected spaces)

--- tests/api/articles.smoke.spec.ts
+++ tests/api/articles.smoke.spec.ts
@@ -1,11 +1,10 @@
 import { expect, test } from '@playwright/test'
 
-test.describe('Verify articles API endpoints', () => {
+test.describe('Verify articles API endpoints @GAD-R08-01 @api', () => {
   test('GET articles returns status code 200 ', async ({ request }) => {
     // Arrange
     const expectedStatusCode = 200
     const articlesUrl = '/api/articles'
-
     // Act
     const response = await request.get(articlesUrl)
     //Assert
