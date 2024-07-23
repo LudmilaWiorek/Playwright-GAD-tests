f06aba8356c78c78b4da26d9cb911a521d54ecb6
LudmilaWiorek <ludmila.wiorek@gmail.com>
2024-07-23 14:08:35 +0200

    test: api smoke articles extended test

--- tests/api/articles.smoke.spec.ts
+++ tests/api/articles.smoke.spec.ts
@@ -10,4 +10,20 @@ test.describe('Verify articles API endpoints @GAD-R08-01 @api', () => {
     //Assert
     expect(response.status()).toBe(expectedStatusCode)
   })
+
+  test('GET articles should return at least one article @predefined_data', async ({
+    request,
+  }) => {
+    // Arrange
+    const expectedMinArticleCount = 1
+    // useful tip: check for numbers 1000 or on empty database [in our case: there is a bookmark in application that reset database]
+    const articlesUrl = '/api/articles'
+    // Act
+    const response = await request.get(articlesUrl)
+    const responseJSON = await response.json()
+    // Assert
+    expect([responseJSON].length).toBeGreaterThanOrEqual(
+      expectedMinArticleCount,
+    )
+  })
 })
